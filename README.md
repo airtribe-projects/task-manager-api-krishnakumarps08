ab5a03502aa7d55b820aa03b7b6e9e2116ddea12
krishnakumarps08 <krishna.k@ampcome.com>
2025-03-06 00:04:52 +0530

    assignment completed successfully

---
+++ README.md
@@ -0,0 +1,12 @@
+clone the repository : git clone <repository_url>
+install dependencies : npm install
+start the server: npm start
+
+
+API Endpoints:
+
+Get All tasks : GET /tasks
+Get Task by Id : GET /tasks/:id
+create new Task : POST /tasks
+update Task : PUT /tasks/:id
+Delete Task : DELETE /tasks/:id
--- app.js
+++ app.js
@@ -1,9 +1,83 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const tasks = require("./task.json")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// console.log(tasks.tasks[0])
+
+app.get("/tasks", (req, res) =>{
+
+    res.send(tasks.tasks)
+})
+
+
+app.get("/tasks/:id", (req, res) =>{
+
+    const id = parseInt(req.params.id)
+    const task = tasks.tasks.find((t) => t.id === id);
+    if (!task) {
+        return res.status(404).json({ error: "Task not found" }); // ✅ Proper 404 response
+    }
+
+    res.json(task);
+})
+
+app.post("/tasks", (req, res) =>{
+
+    const {title, description, completed= false} = req.body
+
+    if(!title || !description){
+        return res.status(400).json({error:"Title and description are required."})
+    }
+
+    if(typeof completed !== "boolean"){
+        return res.status(400).json({ error: "Completed status must be a boolean." });
+    }
+    
+    const newTask = {
+        id: tasks.tasks.length +1,
+        title,
+        description,
+        completed
+    }
+    tasks.tasks.push(newTask)
+    res.status(201).json(newTask);
+})
+
+app.put("/tasks/:id", (req, res) =>{
+    const id = parseInt(req.params.id);
+    const task = tasks.tasks.find((t) => t.id === id);
+    if (!task) {
+        return res.status(404).json({ error: "Task not found" }); // ✅ Proper 404 response
+    }
+    const { title, description, completed } = req.body;
+    if (!title || !description) {
+        return res.status(400).json({ error: "Title and description are required." });
+    }
+    if (typeof completed !== "boolean") {
+        return res.status(400).json({ error: "Completed status must be a boolean." });
+    }
+    task.title = req.body.title
+    task.description = req.body.description
+    task.completed = req.body.completed
+
+
+    res.send(task)
+})
+
+app.delete("/tasks/:id" , (req, res) =>{
+    const id = parseInt(req.params.id);
+    const task = tasks.tasks.find((t) => t.id === id);
+    if (!task) {
+        return res.status(404).json({ error: "Task not found" }); // ✅ Proper 404 response
+    }
+    const taskIndex = tasks.tasks.indexOf(task)
+    tasks.tasks.splice(taskIndex, 1)
+    res.send("deleted successfully")
+})
 
 app.listen(port, (err) => {
     if (err) {
